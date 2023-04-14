38f33febd0e1fde16c3c9bde89a817fd577d1775
GeovaniAndrade <geovanefloripa@gmail.com>
2023-04-14 14:44:56 -0300

    atualizacao da rota para receber id

--- src/app/services/paciente/storage-paciente.service.ts
+++ src/app/services/paciente/storage-paciente.service.ts
@@ -4,53 +4,53 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StoragePacienteService {
-  id:number = 0;
+  id: number = 0;
   constructor() {
-    this.id = (this.getPacientes('PACIENTES')).length;
+    this.atualizarId();
   }
 
   setPacientes(chave: string, paciente: any) {
     let pacientes = this.getPacientes(chave);
     let checarPaciente;
-
-      if(paciente.id == null){
-        paciente.id = this.id;
-        this.atualizarId();
-      }
-      pacientes.push(paciente);
-      localStorage.setItem(chave, JSON.stringify(pacientes));
-
-
+    if (paciente.id == null) {
+      paciente.id = this.id;
+      this.atualizarId();
     }
+    pacientes.push(paciente);
+    localStorage.setItem(chave, JSON.stringify(pacientes));
+  }
 
   getPacientes(chave: string) {
     let pacientesJson = localStorage.getItem(chave)
     let pacientes = pacientesJson ? JSON.parse(pacientesJson) : [];
     return pacientes;
   }
-  atualizarId(){
-    this.id = (this.getPacientes('PACIENTES')).length;
+  atualizarId() {
+    var pacientes = this.getPacientes('PACIENTES');
+    if(pacientes.length != 0){
+      this.id = pacientes[pacientes.length - 1].id + 1
+    console.log(this.id);
+    }
+
   }
 
-  deletarPaciente(chave:string,paciente: any) {
+  deletarPaciente(chave: string, paciente: any) {
     let pacientes = this.getPacientes(chave);
-    pacientes.forEach((element:any,index:number) => {
-      if(paciente.id == element.id){
-        pacientes.splice(index,1);
+    pacientes.forEach((element: any, index: number) => {
+      if (paciente.id == element.id) {
+        pacientes.splice(index, 1);
       }
     });
     localStorage.setItem(chave, JSON.stringify(pacientes));
     this.atualizarId();
   }
 
-  editarPaciente(chave: string,paciente: any){
+  editarPaciente(chave: string, paciente: any) {
     console.log(paciente.id);
     let pacientes = this.getPacientes(chave);
-    console.log(pacientes);
-
-    pacientes.forEach((element:any,index:number)=> {
-      if(element.id==paciente.id){
-        pacientes[index]=paciente;
+    pacientes.forEach((element: any, index: number) => {
+      if (element.id == paciente.id) {
+        pacientes[index] = paciente;
       }
     });
     console.log(pacientes);
