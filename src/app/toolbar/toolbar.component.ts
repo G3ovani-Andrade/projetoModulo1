15ce45b5927c385dc2a3e88a20edbd9a204edeae
GeovaniAndrade <geovanefloripa@gmail.com>
2023-04-18 14:02:49 -0300

    corrigido nome usuario toolbar

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { StorageService } from '../services/storage.service';
-import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute,NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,22 +11,24 @@ export class ToolbarComponent implements OnInit{
   usuarioLogado:any = {};
   login = false;
   @Input() titulo = ''
-  constructor(private serviceLogin:StorageService,private route:Router,private rotaAtive:ActivatedRoute) {}
-  ngOnInit(){
-    this.login = false
-      this.usuarioLogado = this.serviceLogin.getUsuarioLogado('USUARIO_LOGADO');
-      if(this.usuarioLogado.email){
-        this.usuarioLogado.email = this.usuarioLogado.email.split("@",1)
-      }
+  nomeUsuario:string = '';
+  constructor(private serviceLogin:StorageService,private route:Router,private rotaAtive:ActivatedRoute) {
+  }
+  ngOnInit(): void {
+      this.login = false
       this.route.events.subscribe((event)=>{
         if(event  instanceof NavigationEnd){
           if(event.url == "/login" || event.url =="/"){
             this.login = true;
           }else{
+            this.usuarioLogado = this.serviceLogin.getUsuarioLogado('USUARIO_LOGADO');
+            if(this.usuarioLogado.email != undefined)this.nomeUsuario = this.usuarioLogado.email.split('@',1)
             this.login = false;
           }
         }
       })
+
   }
+
 
 }
