d69455180107da05c48900037a33779cbe0baec9
GeovaniAndrade <geovanefloripa@gmail.com>
2023-04-18 11:54:53 -0300

    criacao menu lateral e funcao sair

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'projetoModulo1';
-  titulo ="Estatisticas e Informacoes"
+  titulo =""
+  constructor(private route: Router) {
+
+  }
+  ngOnInit(): void {
+    this.route.events.subscribe((event)=>{
+      if(event  instanceof NavigationEnd){
+        var atual=[];
+        this.titulo = event.url
+        this.titulo = this.titulo.split('/',2)[1];
+      }
+    })
+  }
   pegarTitulo(event:string){
     this.titulo = event;
     console.log(event);
   }
+
 }
